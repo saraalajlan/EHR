4a6f12d3c6a5892af64c929f6c74a36594f5fef8
abeeraljaloud <abeerjaloud>
2019-04-15 09:46:27 +0300

    Correcting some packages names

---
+++ ehrform/src/main/java/com/ehrform/service/PatientService.java
@@ -0,0 +1,9 @@
+package com.ehrform.service;
+
+import java.util.List;
+import com.ehrform.entity.Patient;
+
+public interface PatientService {
+	public void addPateint(Patient patient);
+	public List<Pateint> getPatient();
+}
